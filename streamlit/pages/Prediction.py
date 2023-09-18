0f2249cbc153b3dc4b65667c55ccb166c666e545
Lala <83470800+lalahashimli@users.noreply.github.com>
2023-09-18 22:55:03 +0400

    Update Prediction.py

--- streamlit/pages/Prediction.py
+++ streamlit/pages/Prediction.py
@@ -5,7 +5,6 @@ import warnings
 import pickle
 import numpy as np
 import time
-from xgboost import XGBRegressor
 from sklearn.model_selection import RepeatedKFold,RepeatedStratifiedKFold,StratifiedKFold,train_test_split,GridSearchCV,cross_val_score
 from sklearn.preprocessing import StandardScaler , RobustScaler, MaxAbsScaler,MinMaxScaler,OneHotEncoder, LabelEncoder
 from sklearn.feature_selection import SequentialFeatureSelector
